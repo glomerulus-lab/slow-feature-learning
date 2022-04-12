b6061fc8d00c5498d1b76bd5e67aa503b0aff0b6
CgKaminski <cameronkaminski444@gmail.com>
2022-04-11 22:38:02 -0700

    Changed epochs to 3000

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
         "Regular Learning Rate": 0.01,
         "Slow Learning Rate": 0.001,
         "Batch Size": 200,
-        "Epochs": 2000
+        "Epochs": 3000
     }
 
     # Initializing Model
