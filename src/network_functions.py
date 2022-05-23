59584df0c3716fa343403160eaa7fe55ab946e21
CgKaminski <cameronkaminski444@gmail.com>
2022-05-22 22:10:43 -0700

    Changed to recording accuracy directly.

--- src/network_functions.py
+++ src/network_functions.py
@@ -81,7 +81,7 @@ def check_accuracy(device, model, loader, values=list(range(10))):
             num_correct += (predictions == y).sum()
             num_samples += predictions.size(0)
 
-    return 100. * num_correct / num_samples
+    return num_correct / num_samples
 
 
 def classify_targets(targets, values):
