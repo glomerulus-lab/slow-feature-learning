9aa83bedc4924019be4da219e6ac81daf47a0ccf
Cameron Kaminski <kaminsc@wwu.edu>
2022-11-03 21:30:37 -0700

    Ran model.

--- src/job.sh
+++ src/job.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-digits=(98 27 38)
+digits=(01 27 89 69)
 for val in ${digits[@]}; 
 do
-    echo $val
-    python main.py $val 2000 3000 200 0.01 0.0001
+    python main.py $val 2000 3000 200 0.01 0.001
     python main.py $val 2000 3000 200 0.01 0.01
 done
