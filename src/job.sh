9df5443e78276b25361ec7e97abd977418e8c217
CgKaminski <cameronkaminski444@gmail.com>
2022-11-14 10:45:43 -0800

    Changed the shell script for running different learning rates on digits [01 27 89 46].

--- src/job.sh
+++ src/job.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-digits=(01 27 89)
+digits=(01 27 89 46)
 for val in ${digits[@]}; 
 do
+    python main.py $val 2000 3000 200 0.1 0.01
     python main.py $val 2000 3000 200 0.01 0.001
-    python main.py $val 2000 3000 200 0.01 0.01
+    python main.py $val 2000 3000 200 0.9 0.09
+    python main.py $val 2000 3000 200 0.2 0.1
 done
