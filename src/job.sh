db1ad1db2fa14af3700efb551ed993782f5c3419
Cameron Kaminski <kaminsc@wwu.edu>
2022-11-15 13:30:43 -0800

    Previous push set records to save to main file, cleaned that error.

--- src/job.sh
+++ src/job.sh
@@ -4,6 +4,4 @@ for val in ${digits[@]};
 do
     python main.py $val 2000 3000 200 0.1 0.01
     python main.py $val 2000 3000 200 0.01 0.001
-    python main.py $val 2000 3000 200 0.9 0.09
-    python main.py $val 2000 3000 200 0.2 0.1
 done
