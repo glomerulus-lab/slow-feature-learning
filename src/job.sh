d7a8963fac40813ca10c3d3b5613b6554439bdd4
Cameron Kaminski <kaminsc@wwu.edu>
2022-11-29 13:28:53 -0800

    Chnged the running program.

--- src/job.sh
+++ src/job.sh
@@ -3,5 +3,22 @@ digits=(01 27 89 46)
 for val in ${digits[@]}; 
 do
     python main.py $val 2000 3000 200 0.1 0.01
+    python main.py $val 2000 3000 200 0.1 0.001
+    python main.py $val 2000 3000 200 0.1 0.1
+    python main.py $val 2000 3000 200 0.1 0.9
+
+    python main.py $val 2000 3000 200 0.01 0.01
     python main.py $val 2000 3000 200 0.01 0.001
+    python main.py $val 2000 3000 200 0.01 0.1
+    python main.py $val 2000 3000 200 0.01 0.9
+
+    python main.py $val 2000 3000 200 0.001 0.01
+    python main.py $val 2000 3000 200 0.001 0.001
+    python main.py $val 2000 3000 200 0.001 0.1
+    python main.py $val 2000 3000 200 0.001 0.9
+
+    python main.py $val 2000 3000 200 0.9 0.01
+    python main.py $val 2000 3000 200 0.9 0.001
+    python main.py $val 2000 3000 200 0.9 0.1
+    python main.py $val 2000 3000 200 0.9 0.9
 done
