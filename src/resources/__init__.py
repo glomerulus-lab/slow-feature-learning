acae3bc34c5be4ee1b0623d507bae772b0aae40e
Cameron Kaminski <kaminsc@wwu.edu>
2023-04-07 11:28:05 -0700

    removed deleted files from __init__ resource file

--- src/resources/__init__.py
+++ src/resources/__init__.py
@@ -6,8 +6,7 @@
 from .network import NN
 from .network_functions import *
 from .argparser import parseHyperparams
-from .kernel_calc import *
+from .cka_calc import *
 from .load_model import *
-from .save_state_search import *
 
 print("Resources Loaded")
