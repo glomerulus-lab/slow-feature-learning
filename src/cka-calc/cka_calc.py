77a33746bdaca8ae5d22d1c1fcacc123b0c5bb8a
Cameron Kaminski <kaminsc@wwu.edu>
2023-05-18 16:33:09 -0700

    UPDATE cka_calc.py, fully implemented program (ready for cluster runs)

--- src/cka-calc/cka_calc.py
+++ src/cka-calc/cka_calc.py
@@ -9,9 +9,17 @@ CREATED: 05.14.2023
 LAST MODIFIED: 05.14.2023
 """
 
-#import utils
 import argparse
+from typing import List
+
+import torch
 import os
+
+# importing src utils
+import sys
+sys.path.append('../')
+import utils
+
 
 def parse_parameters():
     """ Takes the system arguments and return a dictionary of hyperparameters
@@ -22,7 +30,7 @@ def parse_parameters():
                         help='MNIST digit pair to use for training and testing')
     return parser.parse_args()
 
-def get_model_paths(digits: str) -> str:
+def get_model_paths(digits: str) -> list[str]:
     """ Loads the model from the specified path
     :param PATH: path to the model
     :return: model paths
@@ -35,12 +43,43 @@ def get_model_paths(digits: str) -> str:
         target_path = os.path.join(model_dir, target_dir)
         model_files = os.listdir(target_path)
         for model_file in model_files:
-            model_path = os.path.join(target_path, model_file)
-            model_paths.append(model_path)
-    return model_paths
+            if model_file.endswith(".pt"):
+                model_path = os.path.join(target_path, model_file)
+                model_paths.append(model_path)
+    return sorted(model_paths)
 
 if __name__ == "__main__":
+    # Hyper params
     args = parse_parameters()
-    model_dir = get_model_paths(args.mnistDigits)
-    print(model_dir)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+    # Loading the MNIST dataset for the digit pairs
+    values = [int(char) for char in args.mnistDigits]
+    mnist = utils.load_mnist_dataset(batch_size=0, values=[int(char) for char in args.mnistDigits])
+
+    # Extracting the targets & data
+    data, targets = next(iter(mnist))
+    data = torch.squeeze(data, dim=1)
+    data = data.view(data.size(0), -1)
+
+    # Preparing the targets (y) vector for cka calculation
+    y = torch.unsqueeze(targets.T, -1).float().to(device)
+    y_c = utils.vector_centering(y)
+    y_c_norm = torch.norm(y_c)
+
+    # Loading all model paths (for the specified digit pair)
+    model_dir = get_model_paths(args.mnistDigits)
+    #model_dir.sort()
+    for model_path in model_dir:
+
+        # Loading the model
+        model = utils.load_model(model_path, device)
+
+        # Prepare the model features' matrix (phi) for cka calculation
+        phi = model.features(data)
+        phi_c = utils.matrix_centering(phi)
+
+        # Calculating the cka
+        v = phi_c.T @ y_c
+        cka = (v.T @ v) / (y_c_norm**2 * torch.norm(phi_c.T @ phi_c))
+        print(f"CKA {model_path}: {cka.item()}")
