deaa855b023ac7f629b567a22c69e3edd0b8b961
CgKaminski <cameronkaminski444@gmail.com>
2023-05-30 22:37:43 -0700

    ADDED kulshan job for running program just on the kulshan computer

---
+++ src/cka-calc/kulshan_cka_calc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Start a new screen session named "cka-calc"
+screen -dmS cka-calc
+
+# Activate the virtual environment
+source slow-feature-learning-venv/bin/activate
+
+# Change dir to the location of the python program
+cd src/cka-calc
+
+# Loop through each pair of digits
+for ((i=0; i<=8; i++)); do
+    for ((j=i+1; j<=9; j++)); do
+        # Combine the digits to form a digit pair string
+        digits=$(printf "%d%d" $i $j)
+
+        # Run your Python program with the digit pair string as an argument
+        python cka_calc.py --mnistDigits "$digits"
+    done
+done
