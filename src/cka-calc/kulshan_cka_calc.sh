29b61d224f8cb269110108edc76ff2c966acd72c
CgKaminski <cameronkaminski444@gmail.com>
2023-05-30 22:57:01 -0700

    UPDATED script to create venv

--- src/cka-calc/kulshan_cka_calc.sh
+++ src/cka-calc/kulshan_cka_calc.sh
@@ -3,6 +3,11 @@
 # Start a new screen session named "cka-calc"
 screen -dmS cka-calc
 
+python3 -m venv slow-feature-learning-venv
+activate slow-feature-learning-venv/bin/activate
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+
+
 # Activate the virtual environment
 source slow-feature-learning-venv/bin/activate
 
