1ca5ba27326d120a6402d751e25992b1707c6546
CgKaminski <cameronkaminski444@gmail.com>
2023-05-30 22:21:16 -0700

    UPDATED cka-calc for turning the output into a csv file

--- src/cka-calc/cka_calc.py
+++ src/cka-calc/cka_calc.py
@@ -10,8 +10,9 @@ LAST MODIFIED: 05.14.2023
 """
 
 import argparse
-from typing import List
-
+import csv
+from typing import List, Tuple
+import re
 import torch
 import os
 
@@ -30,7 +31,7 @@ def parse_parameters():
                         help='MNIST digit pair to use for training and testing')
     return parser.parse_args()
 
-def get_model_paths(digits: str) -> list[str]:
+def get_model_paths(digits: str) -> list[tuple[str, list[str]]]:
     """ Loads the model from the specified path
     :param PATH: path to the model
     :return: model paths
@@ -38,6 +39,7 @@ def get_model_paths(digits: str) -> list[str]:
     model_dir = os.path.join(os.getcwd(), "../model-saves")
     all_dirs = os.listdir(model_dir)
     target_dirs = [d for d in all_dirs if d.endswith(digits)]
+    data = []
     model_paths = []
     for target_dir in target_dirs:
         target_path = os.path.join(model_dir, target_dir)
@@ -45,10 +47,14 @@ def get_model_paths(digits: str) -> list[str]:
         for model_file in model_files:
             if model_file.endswith(".pt"):
                 model_path = os.path.join(target_path, model_file)
-                model_paths.append(model_path)
-    return sorted(model_paths)
+                model_paths.append((model_path,
+                    target_dir.split("_") + re.findall(r'\d+', model_file)))
+    model_paths_sorted = sorted(model_paths, key=lambda x: x[1][0])
+    return model_paths_sorted
+
 
 if __name__ == "__main__":
+
     # Hyper params
     args = parse_parameters()
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -70,16 +76,26 @@ if __name__ == "__main__":
     # Loading all model paths (for the specified digit pair)
     model_dir = get_model_paths(args.mnistDigits)
     #model_dir.sort()
-    for model_path in model_dir:
 
-        # Loading the model
-        model = utils.load_model(model_path, device)
+    # Calculating the cka for each model
+    file_name = f"cka_results_{args.mnistDigits}.csv"
+    with open(file_name, mode="w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["input_lr", "readout_lr", "digit_pair", "epoch", "CKA"])
+        t = 0
+        for model_path, hyper_params in model_dir:
+            # Loading the model
+            model = utils.load_model(model_path, device)
 
-        # Prepare the model features' matrix (phi) for cka calculation
-        phi = model.features(data)
-        phi_c = utils.matrix_centering(phi)
+            # Prepare the model features' matrix (phi) for cka calculation
+            phi = model.features(data)
+            phi_c = utils.matrix_centering(phi)
 
-        # Calculating the cka
-        v = phi_c.T @ y_c
-        cka = (v.T @ v) / (y_c_norm**2 * torch.norm(phi_c.T @ phi_c))
-        print(f"CKA {model_path}: {cka.item()}")
+            # Calculating the cka
+            v = phi_c.T @ y_c
+            cka = (v.T @ v) / (y_c_norm ** 2 * torch.norm(phi_c.T @ phi_c))
+            print(f"CKA {model_path}: {cka.item()}")
+            writer.writerow([hyper_params[0], hyper_params[1], hyper_params[2], hyper_params[3], cka.item()])
+            t += 1
+            if t == 10:
+                break
